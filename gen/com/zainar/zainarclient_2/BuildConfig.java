e3b2e975aca1dd8bdda50f261f5a01489e41e5ac
wdiyou <wdiyou@ufl.edu>
2012-07-24 17:47:31 -0400

    07/24/2012

---
+++ gen/com/zainar/zainarclient_2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zainar.zainarclient_2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/zainar/zainarclient_2/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.zainar.zainarclient_2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_welcome, menu);
+        return true;
+    }
+
+    
+}
